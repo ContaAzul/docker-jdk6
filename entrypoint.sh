b6cc5cd8306170c372d7c7dd007564927c6bdbd9
Carlos Alexandro Becker <caarlos0@gmail.com>
2015-05-06 00:34:28 -0300

    fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -eo pipefail
+update-java-alternatives -s "java-${JAVA_VERSION}-oracle"
 service postgresql start
-update-java-alternatives -s java-${JAVA_VERSION}-oracle
+echo "$TZ" | sudo tee /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
 exec "$@"
